bf963bbe2f58d81adaefcecfb83257faa0467e89
muzakparov <b.muzakparov@newagesol.com>
2019-12-07 11:58:51 +0200

    add btn

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+export const Button = (props) => {
+  return (
+  <button className="btn">{props.children}</button>
+  )
+}
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Click</Button>
     </div>
   );
 }
